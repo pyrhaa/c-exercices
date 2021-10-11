79f8890cce10709c21673f3f801fa0312d06569f
pyrhaa <sboubtane@protonmail.com>
2021-10-11 11:20:13 +0200

    ex3 end

---
+++ ex3.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+void ft_print_numbers(void)
+{
+    char c = '0';
+
+    while (c <= '9')
+    {
+        write(1, &c, 1);
+        c++;
+    }
+    write(1, "\n", 1);
+}
+
+int main()
+{
+    ft_print_numbers();
+    return 0;
+}
