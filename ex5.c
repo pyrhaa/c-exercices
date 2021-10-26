1d3cb3e70ab2d1a121de42b2758c280802b7ae3b
pyrhaa <sboubtane@protonmail.com>
2021-10-26 12:40:17 +0200

    ex5 end

--- ex5.c
+++ ex5.c
@@ -21,8 +21,11 @@ void ft_print_comb(void)
                 ft_putchar(a);//a=6 6
                 ft_putchar(b);//b=7 7
                 ft_putchar(c);//c=8 9
-                ft_putchar(',');
-                ft_putchar(' ');
+                if (a != '7')
+                {
+                    ft_putchar(',');
+                    ft_putchar(' ');
+                }
                 c++;
             }
             b++;//b=8
