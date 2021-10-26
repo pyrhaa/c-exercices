489f838f3f286c7265a7ddf49ff8cac92fd3c0b1
pyrhaa <sboubtane@protonmail.com>
2021-10-26 12:27:54 +0200

    ex5 code ok need finition

--- ex5.c
+++ ex5.c
@@ -12,24 +12,24 @@ void ft_print_comb(void)
     char b = '1';
     char c = '2';
 
-    while (a <= '7')
+    while (a <= '7')// a=6
     {
-        while(b <= '8')
+        while(b <= '8')// b=7
         {
-            while (c <= '9')
+            while (c <= '9') //c=10
             {
-                ft_putchar(a);
-                ft_putchar(b);
-                ft_putchar(c);
+                ft_putchar(a);//a=6 6
+                ft_putchar(b);//b=7 7
+                ft_putchar(c);//c=8 9
                 ft_putchar(',');
                 ft_putchar(' ');
                 c++;
             }
-        
-            b++;
-            c= b + 1;
-        
+            b++;//b=8
+            c= b + 1;//c=9
         }
+        b= a + 1;//b=7
+        a++;
     }
     
 }
