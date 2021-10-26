9279b6224a3339fa49036025b4d9d3db695a08e9
pyrhaa <sboubtane@protonmail.com>
2021-10-26 12:01:27 +0200

    ex5 in progress almost finish maybe

--- ex5.c
+++ ex5.c
@@ -11,32 +11,28 @@ void ft_print_comb(void)
     char b = '1';
     char c = '2';
 
-    while (a < b < c)
+    while (a <= '7')
     {
-        while(c <= '9' && b <= '8')
+        while(b <= '8')
         {
-            c++;
-            if(c == '9')
+            while (c <= '9')
             {
-                b++;
-                c = b + 1;
+                ft_putchar(a);
+                ft_putchar(b);
+                ft_putchar(c);
+                ft_putchar(',');
+                ft_putchar(' ');
+                c++;
             }
-            if(c == '9' && b == '8')
-            {
-                a++;
-            }
+        
+            b++;
+            c= b + 1;
+        
         }
-
-        b = a + 1;
-        c = a + 1;
-        ft_putchar(a);
-        ft_putchar(b);
-        ft_putchar(c);
-        ft_putchar(',');
-        ft_putchar(' ');
     }
-    ft_putchar('\n');
+    
 }
+
 
 int main()
 {
