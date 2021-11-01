85f1851af7bf60309bac21d1f464a13a2cc49a23
pyrhaa <sboubtane@protonmail.com>
2021-11-01 11:01:27 +0100

    print if nb is 0 or sup 9

--- ex7.c
+++ ex7.c
@@ -7,9 +7,23 @@ void ft_putchar(char c)
 
 void ft_putnbr(int nb)
 {
+	int a = nb;
+
+	if(nb == 0)
+	{
+		ft_putchar('0');
+	}
+
+	while(nb > 9 || nb != 0)
+	{
+		ft_putchar('0' + (a % 10));
+		nb = a / 10;
+	}
+	ft_putchar(nb);
 }
 
 int main()
 {
+	ft_putnbr(0);
 	return 0;
 }
