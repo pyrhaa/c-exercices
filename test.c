50e97699ae0b51e857a409a2452db883bdf3303f
pyrhaa <sboubtane@protonmail.com>
2021-11-03 19:41:40 +0100

    use recursive for resolve problem

--- test.c
+++ test.c
@@ -7,17 +7,17 @@ void ft_putchar(char c)
 
 void ft_putnbr(int nb)
 {
-	if(nb >= 0 && nb <= 9)
+	if (nb < 0)
 	{
-		ft_putchar(nb + '0');
+		nb = -nb;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
 	}
 	else
-	{
-		nb = nb / 10 + '0';
-		ft_putchar(nb);
-		nb = nb % 10 + '0';
-		ft_putchar(nb);
-	}
+		ft_putchar(nb + '0');
 
 }
 
