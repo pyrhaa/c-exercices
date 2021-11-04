2e59f216bd5d9e7ce51a5cc813ba0d10b6b6df7c
pyrhaa <sboubtane@protonmail.com>
2021-11-04 17:18:22 +0100

    not finished, to do tomorrow

--- ex8.c
+++ ex8.c
@@ -5,9 +5,18 @@ void ft_putchar(char c)
     write(1, &c, 1);
 }
 
+void ft_char_number(int nb)
+{
+	ft_putchar(nb / 10 + 48);
+	ft_putchar(nb % 10 + 48);
+}
+
 void ft_print_combn(int n)
 {
-
+	while (n <= 89)
+	{
+		
+	}
 }
 
 int main()
