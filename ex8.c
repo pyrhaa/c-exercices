1e0ad4499c3cd5a219bf52fd668656d9bf3b46a4
pyrhaa <sboubtane@protonmail.com>
2021-11-04 15:11:10 +0100

    initialize ex8

---
+++ ex8.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_print_combn(int n)
+{
+
+}
+
+int main()
+{
+    ft_putchar('@');
+    ft_putchar('\n');
+    return 0;
+}
